f4f415b50525d8195aa089be6e2dc161ebb63002
Kalepu Rajeswari <kalepu.raji@gmail.com>
2025-04-24 09:56:01 +0800

    partial_seq_removal.py

--- Module 2/partial_seq_removal.py
+++ Module 2/partial_seq_removal.py
@@ -1,17 +1,18 @@
-
-# Program to remove the partial protein sequences in multi fasta file
-# Start of the program
+# Script to remove the partial protein sequences in a multi-FASTA file
+# Start of the Script
 
 from Bio import SeqIO
-
 def remove_partial_sequences(input_fasta, output_fasta):
     with open(output_fasta, "w") as out_fh:
         for record in SeqIO.parse(input_fasta, "fasta"):
             if "partial" not in record.description:
                 SeqIO.write(record, out_fh, "fasta")
 
-input_fasta = "C:/Users/INBIOSIS/Desktop/RPW-fasta/Redundancy_removed_sequences.fasta"  # Replace with your actual input file
-output_fasta = "C:/Users/INBIOSIS/Desktop/RPW-fasta/Partial_sequences_removed.fasta"  # Replace with your desired output file
+# input and output files, give path according to your data
+input_fasta = "/home/inbiosis/Documents/Rpw-prot-seq-Redundant-removal.fasta" 
+output_fasta = "/home/inbiosis/Documents/Rpw-prot-seq-partial-removed.fasta" 
+
+#remove partial sequence function call
 remove_partial_sequences(input_fasta, output_fasta)
 
-#End of the program
+# End of the Script
