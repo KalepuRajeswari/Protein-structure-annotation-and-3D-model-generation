d8ed8aaefbfeb574f829c6f78d72fbb50bd64555
Kalepu Rajeswari <kalepu.raji@gmail.com>
2025-04-24 09:47:41 +0800

    combine_fasta_files.py

--- Module 2/combine_fasta_files.py
+++ Module 2/combine_fasta_files.py
@@ -1,22 +1,23 @@
+# Script to combine all downloaded individual fasta files into one multi-FASTA file
+# Start of the Script
+
 from Bio import SeqIO
 import os
-
 def combine_fasta_files(input_files, output_file):
-
     with open(output_file, 'w') as outfile:
         for file in input_files:
             for record in SeqIO.parse(file, "fasta"):
                 SeqIO.write(record, outfile, "fasta")
     print(f"Combined FASTA file created at: {output_file}")
 
-# Specify the directory containing FASTA files (make sure all required fasta files in input folder)
-input_directory = "C:/Users/INBIOSIS/Desktop/RPW-fasta" 
-
-# Specify the output file name
-output_file = "C:/Users/INBIOSIS/Desktop/RPW-fasta/combined_fasta_file.fasta"
+# Specify the input and output directory names
+input_directory = "C:/Users/INBIOSIS/Desktop/Combine-fasta"
+output_file = "C:/Users/INBIOSIS/Desktop/Combine-fasta/Rpw-prot-seq.fasta"
 
 # Gather all FASTA files from the directory
 fasta_files = [os.path.join(input_directory, f) for f in os.listdir(input_directory) if f.endswith(".fasta")]
 
 # Combine the FASTA files
 combine_fasta_files(fasta_files, output_file)
+
+# End of the Script
