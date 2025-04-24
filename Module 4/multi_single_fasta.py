aee8f701db376507fa2f1280f547dff6ee985c14
Kalepu Rajeswari <kalepu.raji@gmail.com>
2025-04-24 10:37:06 +0800

    multi_single_fasta.py

--- Module 4/multi_single_fasta.py
+++ Module 4/multi_single_fasta.py
@@ -1,21 +1,25 @@
+# Python script to separate a multi-FASTA file into single-FASTA files with the protein ID as a file name.
+# Start of the script
+
 from Bio import SeqIO
 import os
-# input file
-#input_file = "C:/Users/INBIOSIS/Desktop/RPW-fasta/protein-multiFastaSeq.fasta"
-input_file = "C:/Users/INBIOSIS/Desktop/RPW_prot/protein-multiFastaSeq.fasta"
 
-output_dir = "C:/Users/INBIOSIS/Desktop/RPW_prot"
-count =0
+# input and output files, give the path according to your data
+input_file = "/home/INBIOSIS/Documents/colab/Rpw-prot-seq.fasta"
+output_dir = "/home/INBIOSIS/Documents/colab/single_files_dir"
+
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Parse the multi-fasta file and save each sequence to a separate file
+# Parse the multi-FASTA file and save each sequence to a separate file
+count =0
 for record in SeqIO.parse(input_file, "fasta"):
     output_file = os.path.join(output_dir, f"{record.id}.fasta")
     with open(output_file, "w") as f:
         SeqIO.write(record, f, "fasta")
         print(output_file, + count)
         count = count + 1;
-    print(f"FASTA file created at: {output_dir}")
+print(f"Total sequences processed: {count}")
 
+# End of the script
